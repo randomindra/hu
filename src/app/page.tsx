1fa8b8be51231b156f08a4b48c5076fd569b917a
indra <indraaroragithub@gmail.com>
2023-08-22 14:32:22 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1>this push will be done  master branch</h1>
+      <h1>this push will be   master branch</h1>
     </div>
   )
 }
